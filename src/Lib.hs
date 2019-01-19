edabc72695f91a17fcf18b81d3fda5293441ad9a
Dreamonic <retep.gnahz@gmail.com>
2019-01-19 20:25:39 +0100

    Add a rudementary lexer

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,11 @@ module Lib
     ( someFunc
     ) where
 
+
+import Lexer
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    putStrLn "Character: "
+    input <- getLine
+    print (tokenizeString input)
