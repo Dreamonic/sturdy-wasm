42f38d20401af9aabcc96f2b3f88a1477aadd19f
Nick Yu <nickyu42@gmail.com>
2019-02-15 00:00:40 +0100

    Add basic file reading in Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,30 @@ module Lib
     ( someFunc
     ) where
 
-
 import Lexer
+import System.Environment
+import Data.List
+import System.IO
 
-someFunc :: IO ()
-someFunc = do
+-- |Executes interactive mode if no argument is given
+-- if a single argument is given, read file and execute
+parseArgs :: [String] -> IO ()
+parseArgs []        = interactive
+parseArgs (arg:[])  = execute arg
+parseArgs args      = putStrLn $ "Invalid Options: " ++ intercalate " " args
+
+-- |Reads WAST user input and executes it
+interactive :: IO ()
+interactive = do 
     putStrLn "Character: "
     input <- getLine
-    print (tokenizeAll input)
+    print $ tokenizeAll input
+
+-- |Reads a file and executes 
+execute :: String -> IO ()
+execute filename = do
+    contents <- readFile filename
+    print $ filter (/= '\n') contents -- TODO: clean up input string
+
+someFunc :: IO ()
+someFunc = parseArgs =<< getArgs
