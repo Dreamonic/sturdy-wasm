9cf18901ea846639e5dd8156fcf8e1a50d3768a1
Dreamonic <retep.gnahz@gmail.com>
2019-03-10 14:57:00 +0100

    Fix error and add functor type

--- src/Parser.hs
+++ src/Parser.hs
@@ -21,16 +21,9 @@ import Text.ParserCombinators.Parsec
 
 import           Control.Monad
 import           Lexer
+import           WasmTypes
 
 import Data.List
-
-
-data WasmType
-  = I32
-  | I64
-  | F32
-  | F64
-  deriving (Show, Eq)
 
 data Instr
   = EnterBlock Block
@@ -59,50 +52,6 @@ data TypedInstr
   | Neg WasmType
   deriving (Show, Eq)
 
-data WasmVal
-  = I32Val Integer
-  | I64Val Integer
-  | F32Val Double
-  | F64Val Double
-  deriving (Show, Eq)
-
-class WasmApplicative a where
-  (<$>) :: (a -> a) -> WasmVal -> WasmVal -> WasmVal
-
-data Applicable a = a :<*> a
-
-infixl 0 <$>
-infixl 1 :<*>
-
-instance WasmApplicative Integer where
-  f <$> (I32Val a) :<*> (I32Val b) = I32Val (f a b)
-  f <$> (I64Val a) :<*> (I64Val b) = I64Val (f a b)
-
-instance WasmApplicative Double where
-  f <$> (F32Val a) :<*> (F32Val b) = F32Val (f a b)
-  f <$> (F64Val a) :<*> (F64Val b) = F64Val (f a b)
-
-getType :: WasmVal -> WasmType
-getType typ = case typ of
-  (I32Val _) -> I32
-  (I64Val _) -> I64
-  (F32Val _) -> F32
-  (F64Val _) -> F64
-
-ofType :: WasmVal -> WasmType -> Bool
-ofType val typ = (getType val) == typ
-
-toWasmI :: WasmType -> Integer -> WasmVal
-toWasmI typ x = case typ of
-  I32 -> I32Val x
-  I64 -> I64Val x
-  _   -> toWasmF typ (fromIntegral x)
-
-toWasmF :: WasmType -> Double -> WasmVal
-toWasmF typ x = case typ of
-  F32 -> F32Val x
-  F64 -> F64Val x
-  _   -> toWasmI typ (round x)
 
 data Param = Param String WasmType deriving (Show, Eq)
 data Result = Result WasmType deriving (Show, Eq)
