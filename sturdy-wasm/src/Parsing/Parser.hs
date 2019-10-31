319d409adc3f1b937a660c49cfcb122bc449dace
Nick Yu <nickyu42@gmail.com>
2019-10-31 19:54:16 +0100

    Remove commented syntax in Parser.hs

--- sturdy-wasm/src/Parsing/Parser.hs
+++ sturdy-wasm/src/Parsing/Parser.hs
@@ -26,59 +26,6 @@ import Parsing.Lexer
 import Types(WasmType(..), WasmVal(..))
 import Syntax
 
--- data Instr
---   = Const WasmVal
---   | Block [WasmType] [Instr]
---   | Br Integer
---   | BrIf Integer
---   | If [WasmType] [Instr] [Instr]
---   | Loop [WasmType] [Instr]
---   | Call String
---   | LocalGet String
---   | LocalSet String
---   | LocalTee String
---   | Binary WasmType BinOpInstr
---   | Unary WasmType UnOpInstr
---   | Compare WasmType RelOpInstr
---   | Nop
---   | Unreachable
---   deriving (Show, Eq)
-
--- data BinOpInstr
---   = Add
---   | And
---   | Sub
---   | Mul
---   | Div SignedNess
---   | Or
---   | Xor deriving (Show, Eq)
-
--- data UnOpInstr
---   = Neg
---   | Abs deriving (Show, Eq)
-
--- data RelOpInstr
---   = Eql deriving (Show, Eq)
-
--- data Param = Param {getName :: String, getValue :: WasmType} deriving (Show, Eq)
--- data Result = Result WasmType deriving (Show, Eq)
--- data SignedNess = Signed | Unsigned deriving (Show, Eq)
--- data Func = Func String [Param] [Result] [Instr] deriving (Show, Eq)
--- data WasmModule = WasmModule [Func] deriving (Show, Eq)
-
--- getResult :: Result -> WasmType
--- getResult (Result t) = t
-
--- isInt :: WasmType -> Bool
--- isInt I32 = True
--- isInt I64 = True
--- isInt _   = False
-
--- isFloat :: WasmType -> Bool
--- isFloat F32 = True
--- isFloat F64 = True
--- isFloat _   = False
-
 -- Parse a function body consisting of a mix of plain and foldedinstructions
 parseBody :: Parser [Instr]
 parseBody = do
