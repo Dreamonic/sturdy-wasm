834962cabaca7ae34e216a936b4b84b100465ca3
Siert Sebus <siertsebus@gmail.com>
2019-12-24 14:37:53 +0100

    Write initial version of generic interp.

--- sturdy-wasm/src/Control/Arrow/Wasm.hs
+++ sturdy-wasm/src/Control/Arrow/Wasm.hs
@@ -7,7 +7,8 @@ module Control.Arrow.Wasm
     ( Frame(..)
     , frVals
     , frInstrs
-    , frData
+    , frRty
+    , frKind
     , Closure
     , closVars
     , closFrs
@@ -15,6 +16,9 @@ module Control.Arrow.Wasm
     , pushVal
     , pushVals
     , popVal
+    , getVals
+    , nextInstr
+    , putInstr
     , pushFr
     , popFr
     , pushClos
@@ -23,37 +27,48 @@ module Control.Arrow.Wasm
     , setLocal
     , setFuncs
     , getFunc
+    , block
+    , loop
     ) where
 
-import Control.Arrow
+import Control.Arrow hiding (loop)
 import Data.Profunctor
 import qualified Data.Map as M
 import Control.Lens.TH
 
 import Syntax
+import Types
 
-data Frame v fd = Frame { _frVals :: [v]
-                        , _frInstrs :: [Instr]
-                        , _frData :: fd }
-data Closure v fd = Closure { _closVars :: M.Map String v
-                            , _closFrs :: [Frame v fd] }
+data FrameKind = BlockK | LoopK [Instr]
+
+data Frame v = Frame { _frVals :: [v]
+                     , _frInstrs :: [Instr]
+                     , _frRty :: [WasmType]
+                     , _frKind :: FrameKind }
+
+data Closure v = Closure { _closVars :: M.Map String v
+                         , _closFrs :: [Frame v] }
 
 makeLenses ''Frame
 makeLenses ''Closure
 
-class (ArrowChoice c, Profunctor c) => ArrowWasm v fd c | c -> v, c -> fd where
+class (ArrowChoice c, Profunctor c) => ArrowWasm v c | c -> v where
     pushVal :: c v ()
-    pushVals :: c [v] ()
-    pushVals = proc x -> case x of
-        []   -> returnA -< ()
-        v:vs -> do pushVal -< v
-                   pushVals -< vs
     popVal :: c () v
-    pushFr :: c (Frame v fd) ()
-    popFr :: c () (Frame v fd)
-    pushClos :: c (Closure v fd) ()
-    popClos :: c () (Closure v fd)
+    getVals :: c () [v]
+    nextInstr :: c () (Maybe Instr)
+    putInstr :: c Instr ()
+    pushFr :: c (Frame v) ()
+    popFr :: c () (Maybe (Frame v))
+    pushClos :: c (Closure v) ()
+    popClos :: c () (Maybe (Closure v))
     getLocal :: c String v
     setLocal :: c (String, v) ()
     setFuncs :: c (M.Map String Func) ()
     getFunc :: c String Func
+
+block :: ArrowWasm v c => c ([WasmType], [Instr]) ()
+block = proc (rtys, is) -> pushFr -< (Frame [] is rtys BlockK)
+
+loop :: ArrowWasm v c => c ([WasmType], [Instr]) ()
+loop = proc (rtys, is) -> pushFr -< (Frame [] is rtys (LoopK is))
